397fd50c78a1873d363cbfed8b5d04532dfb91dc
kmuehlenberg <muehlenbergz55@gmail.com>
2025-07-06 00:12:19 -0700

    Update drops.js

--- pages/drops.js
+++ pages/drops.js
@@ -1,1 +1,8 @@
-// drops.js placeholder
+export default function Drops() {
+  return (
+    <div>
+      <h1>Vault Drops</h1>
+      <p>Exclusive bundles and limited product releases.</p>
+    </div>
+  );
+}
