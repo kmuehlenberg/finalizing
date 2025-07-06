5af6b2808fe1c9acd808ada3f106a0c621736645
kmuehlenberg <muehlenbergz55@gmail.com>
2025-07-06 00:12:02 -0700

    Update concierge.js

--- pages/concierge.js
+++ pages/concierge.js
@@ -1,1 +1,8 @@
-// concierge.js placeholder
+export default function Concierge() {
+  return (
+    <div>
+      <h1>Concierge Page</h1>
+      <p>This will soon host the AI gift assistant.</p>
+    </div>
+  );
+}
