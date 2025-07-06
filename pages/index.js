94118a3cc50a28f47fb41238e73d5eccbd2d01c3
kmuehlenberg <muehlenbergz55@gmail.com>
2025-07-06 00:09:34 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Valestra</h1>
+      <p>This is your fully functional luxury e-commerce homepage.</p>
+    </main>
+  );
+}
